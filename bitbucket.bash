682d61da096debd411f5db4a539cd945a18e568a
Tahseen Jamal <tahseen.jamal@gmail.com>
2018-10-13 11:09:02 +0530

    Added pretty json output

--- bitbucket.bash
+++ bitbucket.bash
@@ -1,1 +1,1 @@
-curl -X POST -v -u $1:$2 -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$3/$4 -d '{"scm": "git", "is_private": "true", "fork_policy": "no_public_forks" }'
+curl -X POST -v -u $1:$2 -H "Content-Type: application/json" https://api.bitbucket.org/2.0/repositories/$3/$4 -d '{"scm": "git", "is_private": "true", "fork_policy": "no_public_forks" }' | python -m json.tool
